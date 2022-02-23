28ce18d3605556046928f590fa2cc15aca850a70
Kerebel <bastienkerebel@gmail.com>
2022-02-23 10:52:45 +0100

    Classe Port commencee

---
+++ src/Entity/Port.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PortRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PortRepository::class)
+ */
+class Port
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $indicatif;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Pays::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $lePays;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getIndicatif(): ?string
+    {
+        return $this->indicatif;
+    }
+
+    public function setIndicatif(string $indicatif): self
+    {
+        $this->indicatif = $indicatif;
+
+        return $this;
+    }
+
+    public function getLePays(): ?Pays
+    {
+        return $this->lePays;
+    }
+
+    public function setLePays(?Pays $lePays): self
+    {
+        $this->lePays = $lePays;
+
+        return $this;
+    }
+}
